958a01da26e80dde2ebcb4ae581b0ea2149aac19
mark <markhinojosa1212@gmail.com>
2019-07-29 21:58:29 -0700

    added things

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,11 +1,97 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import Chatkit from '@pusher/chatkit-client';
+import WhosOnlineList from './WhosOnlineList';
 
 export default class ChatScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: {},
+            currentRoom: {},
+            messages: [],
+            usersWhoAreTyping: [],
+        }
+        // this.sendMessage = this.sendMessage.bind(this)
+        // this.sendTypingEvent = this.sendTypingEvent.bind(this)
+    }
+
+    componentDidMount() {
+        // const chatManager = new Chatkit.ChatManager({
+        //     instanceLocator: 'v1:us1:a8cbb16b-0c43-46f6-851b-976d04bb843b',
+        //     userId: this.props.currentUsername,
+        //     tokenProvider: new Chatkit.TokenProvider({
+        //         url: 'http://localhost:3001/authenticate'
+        //     })
+        // })
+
+        // chatManager
+        //     .connect()
+        //     .then(currentUser => {
+        //         this.setState({ currentUser })
+        //         return currentUser.subscribeToRoom({
+        //             roomId: '19893490',
+        //             messageLimit: 100,
+        //             hooks: {
+        //                 onMessage: message => {
+        //                     this.setState({
+        //                         messages: [...this.state.messages, message],
+        //                     })
+        //                 },
+        //                 onUserStartedTyping: user => {
+        //                     console.log(user);
+        //                     this.setState({
+        //                         usersWhoAreTyping: [...this.state.usersWhoAreTyping, user.name],
+        //                     }, () => console.log(this.state.usersWhoAreTyping))
+        //                 },
+        //                 onUserStoppedTyping: user => {
+        //                     this.setState({
+        //                         usersWhoAreTyping: this.state.usersWhoAreTyping.filter(
+        //                             username => username !== user.name
+        //                         ),
+        //                     })
+        //                 },
+        //             },
+        //         })
+        //     })
+        //     .then(currentRoom => {
+        //         this.setState({ currentRoom }, () => console.log(this.state.currentRoom.id))
+        //     })
+        //     .catch(error => console.error('error', error))
+    }
+
+    // sendMessage(text) {
+    //     this.state.currentUser.sendMessage({
+    //         text,
+    //         roomId: this.state.currentRoom.id
+    //     })
+    // }
+
+    // sendTypingEvent = () => {
+    //     console.log(this.state.currentRoom.id)
+    //     this.state.currentUser
+    //         .isTypingIn({ roomId: this.state.currentRoom.id })
+    //         .catch(error => console.error('error', error))
+    // }
+
     render() {
         return (
             <View>
-                <Text> ChatScreen </Text>
+                <View style={{ width: "100%", height: '100%', borderWidth: 3, borderColor: "black", flexDirection: 'row' }}>
+                    <WhosOnlineList/>
+                    <View>
+                        <Text>message list</Text>
+                    </View>
+                    <View>
+                        <Text>typing indicator</Text>
+                    </View>
+                    <View>
+                        <Text>Send Message form</Text>
+                    </View>
+                </View>
+
+
             </View>
         )
     }
